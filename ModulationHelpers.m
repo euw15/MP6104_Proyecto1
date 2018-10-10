c5f2e256adb4e8113a786518063be2d180d3d7a6
U-TER\umanaedw <umanaedw@umanaed-laptop.ter.teradyne.com>
2018-10-10 13:20:11 -0600

    Write output file, not taking into account the intercept data

---
+++ ModulationHelpers.m
@@ -0,0 +1,15 @@
+function calculateReplicate(amplitude, phase)
+    replicaSampleIndex = windowSample*currentWindowN-phase;
+    if(replicaSampleIndex < 1 ||   replicaSampleIndex > audioInfo.TotalSamples)
+      replicaSample = 0;
+    endif
+    else
+      replicaSample = amplitude*audioSamples(replicaSampleIndex);
+    endif
+    
+    audioSamples(windowSample*currentWindowN)+replicaSample;
+  
+  
+  
+  
+endfunction
--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+clear;
 clc;
 %********************************************************
 %**********   Extraccion de Audio        ****************
@@ -17,42 +18,83 @@ if(n_windows <= 0)
   return;
 endif
 
-windowsSize = audioInfo.TotalSamples/n_windows;
+
 
 %*********************************************************
 %**************    Multiplexado  *************************
 %*********************************************************
-s_a0 = 'a0';
+s_a0 = 'a0 ';
 d_a0 = input(s_a0);
 
-s_a1 = 'a1';
+s_a1 = 'a1 ';
 d_a1 = input(s_a1);
 
-s_b0 = 'b0';
-s_b0 = input(s_b0);
+s_b0 = 'b0 ';
+d_b0 = input(s_b0);
 
-s_b1 = 'b1';
+s_b1 = 'b1 ';
 d_b1 = input(s_b1);
 
-s_metadatos = 'Metadatos';
+s_metadatos = 'Metadatos ';
 d_metadatos = input(s_metadatos);
 v_ascii     = toascii (d_metadatos);
 
-for samplesIndex=1:8
-  
-  index             = fix(abs(samplesIndex-1)/7)+1;
-  currentLetter     = v_ascii(index);
-  currentLetterBits = flip ( bitget (currentLetter, 7:-1:1));
-  currentBit        = currentLetterBits(mod((samplesIndex-1),7)+1);
-  
-  if(currentBit)
-  
-  
-  
-  endif
-  else
+windowsSize = audioInfo.TotalSamples/n_windows;
 
-  endif
-  %y_n = audioSamples(samples)+d_a0*audioSamples(samples-s_b0);
-  %y_n = audioSamples(samples)+d_a1*audioSamples(samples-s_b1);
-endfor
+samplesIndex = 1;
+while(samplesIndex <= audioInfo.TotalSamples)
+    letterIndex  = fix(abs(samplesIndex-1)/7)+1;
+  
+  if(letterIndex > length(v_ascii))
+    y_n(samplesIndex) = audioSamples(samplesIndex);
+    samplesIndex++;
+  else  
+    currentLetter      = v_ascii(letterIndex);
+    currentLetterBits  = flip ( bitget (currentLetter, 7:-1:1));
+    currentBit         = currentLetterBits(mod((samplesIndex-1),7)+1);
+    
+    currentWindowIndex = mod(samplesIndex,n_windows);
+    currentWindowN     = fix(abs(samplesIndex-1)/7)+1;
+    
+    if(currentBit)
+      for windowSample = 1:windowsSize
+          replicaSampleIndex = windowSample*currentWindowN-d_b0;
+          
+          if(windowSample*currentWindowN > audioInfo.TotalSamples)
+            break;
+          endif
+          
+          if(replicaSampleIndex < 1 ||   replicaSampleIndex >= audioInfo.TotalSamples)
+            replicaSample = 0;
+          else
+            replicaSample = d_a0*audioSamples(replicaSampleIndex);
+          endif
+            y_n(samplesIndex) = audioSamples(windowSample*currentWindowN)+replicaSample;
+            samplesIndex++;
+            
+      endfor
+    else
+      for windowSample = 1:windowsSize
+          replicaSampleIndex = windowSample*currentWindowN-d_b1;
+          
+          if(windowSample*currentWindowN > audioInfo.TotalSamples)
+            break;
+          endif
+          
+          if(replicaSampleIndex < 1 ||   replicaSampleIndex >= audioInfo.TotalSamples)
+            replicaSample = 0;
+          else
+            replicaSample = d_a1*audioSamples(replicaSampleIndex);
+          endif
+           y_n(samplesIndex) = audioSamples(windowSample*currentWindowN)+replicaSample;
+           samplesIndex++;
+            
+      endfor
+    endif
+   endif
+  
+endwhile
+
+audiowrite("output.wav",y_n,audioInfo.SampleRate,'BitsPerSample',audioInfo.BitsPerSample);
+
+
